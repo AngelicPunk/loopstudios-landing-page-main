10675df2a9b1f0be85807e798e5543ebe88f5484
AngelicPunk <85429374+AngelicPunk@users.noreply.github.com>
2021-10-24 21:02:19 -0500

    5

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const links = document.querySelector('.links');
 const navlinks = document.querySelectorAll('.links li');
+const animate = document.querySelector(".bar")
+const bar1 = document.querySelector('.bar1')
+const bar2 = document.querySelector('.bar2')
+const bar3 = document.querySelector('.bar3')
 
 hamburger.addEventListener('click', () => {
         links.classList.toggle("links");
-        
     })
-
+ 
