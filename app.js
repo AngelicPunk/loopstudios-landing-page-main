b7e95b4df9b1be54eefff3e50fb9002744973eb6
AngelicPunk <85429374+AngelicPunk@users.noreply.github.com>
2022-02-26 03:27:01 -0600

    yuh

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const links = document.querySelector('.links');
-const navlinks = document.querySelectorAll('.links li');
-const animate = document.querySelector(".bar")
-const bar1 = document.querySelector('.bar1')
-const bar2 = document.querySelector('.bar2')
-const bar3 = document.querySelector('.bar3')
+const animate = document.getElementById("animate");
+const bar1 = document.getElementsByClassName('.bar1');
+const bar2 = document.getElementsByClassName('.bar2');
+const bar3 = document.getElementsByClassName('.bar3');
+const bar = [bar1, bar2, bar3];
 
-hamburger.addEventListener('click', () => {
-        links.classList.toggle("links");
-    })
  
+hamburger.addEventListener('click', () => {
+       hamburger.classList.toggle("active");
+       links.classList.toggle("links");
+ });
+
+ document.querySelectorAll(".links").forEach(n => n.addEventListener("click", () => {
+        hamburger.classList.remove(".active");
+        links.classList.remove("active");
+ }));
